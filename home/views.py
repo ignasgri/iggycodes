e4bf4b3da91e078bcb63793390b0d7588f36b1df
Ignas Grigaravicius <kickeris@gmail.com>
2018-10-03 21:40:40 +0100

    contact form renders in index html

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from contacts.forms import ContactsForm
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {
+        'form': ContactsForm,
+    })
 
 def shop(request):
     return render(request, "shop.html")
