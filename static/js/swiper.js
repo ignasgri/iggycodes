a0ff2dab56def366cf35ddeb012e95964be212eb
Ignas Grigaravicius <kickeris@gmail.com>
2018-11-21 08:09:23 +0000

    slder.js added, images changed, design updated

---
+++ static/js/swiper.js
@@ -0,0 +1,23 @@
+// var swiper1 = new Swiper('.s1', {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     pagination: '.swiper-pagination',
+//     autoplay: '5000',
+//     loop: 'true',
+//     // autoplay: {
+//     //   delay: 5000,
+//     //   disableOnInteraction: false,
+//     // },
+//     // pagination: {
+//     //   el: '.swiper-pagination',
+//     //   clickable: true,
+//     // },
+//   })
+var swiper2 = new Swiper('.s2', {
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: '2000',
+    loop: 'true',
+    effect: 'fade',
+ });
